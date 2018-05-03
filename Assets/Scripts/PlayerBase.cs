e67679faefabb02b52c0484b2c6ae1fb90dfe755
shiren2525 <furainoshiren2525@gmail.com>
2018-05-03 20:18:22 +0900

    PlayerBaseの追加

---
+++ Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour {
+
+    // 操作ボタン
+    private KeyCode keyLeft = KeyCode.LeftArrow;
+    private KeyCode keyRight = KeyCode.RightArrow;
+    private KeyCode keyAttack = KeyCode.Z;
+    private KeyCode keyJump = KeyCode.Space;
+
+    // 移動速度
+    private float speed;
+}
